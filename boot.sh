92e0789f667ccfc68513f546ec3bb5af487eadc5
user <user@github.com>
2025-07-05 22:27:52 +0200

    fixes url

--- boot.sh
+++ boot.sh
@@ -2,7 +2,7 @@ pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
 
 echo -e "\nCloning Hyprarch..."
 rm -rf ~/.local/share/hyprarch/
-git clone https://github.com/basecamp/hyprarch.git ~/.local/share/hyprarch >/dev/null
+git clone https://github.com/snu1v3r/hyprarch.git ~/.local/share/hyprarch >/dev/null
 
 echo -e "\nInstallation starting..."
 source ~/.local/share/hyprarch/install.sh
