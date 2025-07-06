94d1c55c694c813666a9fc4707f901ade2fea85c
user <user@github.com>
2025-07-06 10:39:21 +0200

    minor fixes
    - remove unused applications
    - fixes fonts
    - adds brave and keepassxc

--- install/fonts.sh
+++ install/fonts.sh
@@ -15,6 +15,19 @@ if ! fc-list | grep -qi "CaskaydiaMono Nerd Font"; then
   cd -
 fi
 
+if ! fc-list | grep -qi "JetBrainsMono Nerd Font"; then
+  cd /tmp
+  wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+  unzip JetBrainsMono.zip -d JetBrainsFont
+  cp JetBrainsFont/JetBrainsMonoNerdFont-Regular.ttf ~/.local/share/fonts
+  cp JetBrainsFont/JetBrainsMonoNerdFont-Bold.ttf ~/.local/share/fonts
+  cp JetBrainsFont/JetBrainsMonoNerdFont-Italic.ttf ~/.local/share/fonts
+  cp JetBrainsFont/JetBrainsMonoNerdFont-BoldItalic.ttf ~/.local/share/fonts
+  rm -rf JetBrainsMono.zip JetBrainsFont
+  fc-cache
+  cd -
+fi
+
 if ! fc-list | grep -qi "iA Writer Mono S"; then
   cd /tmp
   wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
