13fdf8f003b706a5d8113a5d31aa6653f8af1404
user <user@github.com>
2025-07-12 22:30:45 +0200

    cleaned various files
    - removed unused bash
    - moved xcompose
    - added tmux, bat-extras

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -1,6 +1,7 @@
 yay -S --noconfirm --needed \
   wget curl unzip inetutils \
-  fd eza fzf ripgrep zoxide bat \
+  fd eza fzf ripgrep zoxide bat batman \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate \
-  alacritty zsh stow
+  alacritty zsh stow \
+  tmux
