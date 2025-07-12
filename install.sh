842004b576e88bba849ff84faab3ae029d1ac19a
user <user@github.com>
2025-07-12 12:55:32 +0200

    cleaner install procedure

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+echo -e "\nInstalling git..."
+pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
+
+echo -e "\nCloning Hyprarch..."
+rm -rf ~/.local/share/hyprarch/
+git clone https://github.com/snu1v3r/hyprarch.git ~/.local/share/hyprarch >/dev/null
+
+echo -e "\nCloning Dotfiles..."
+rm -rf ~/.local/share/dotfiles/
+git clone --recursive --depth 1 https://github.com/snu1v3r/dotfiles.git ~/.local/share/dotfiles >/dev/null
+
+echo -e "\nInstallation starting..."
+
 # Install everything
 for f in ~/.local/share/hyprarch/install/*.sh; do source "$f"; done
 
