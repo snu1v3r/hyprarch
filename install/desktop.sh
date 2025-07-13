2e8427d0639cba4e370c42468236da7f5e13e62b
user <user@github.com>
2025-07-13 18:12:24 +0200

    enabled some applications

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,9 +1,10 @@
 yay -S --noconfirm --needed \
-  keepassxc-qt6
-# brave-bin \
+  keepassxc qt5-wayland \
+  brave-bin \
+  nautilus \
+  gnome-calculator
 # wl-clip-persist clipse \
 # chromium vlc # fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
 # nautilus sushi gnome-calculator \
 #  keepassxc-qt6 #   brightnessctl playerctl pamixer pavucontrol wireplumber \
-#   keepassxc qt5-wayland \
 #   evince imv
