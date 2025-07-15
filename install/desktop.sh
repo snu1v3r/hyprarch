78627ee00829a116f18510c048a13ddc07d725f7
user <user@github.com>
2025-07-15 19:20:37 +0200

    various fixes
    - add needed applications
    - add script for plugin

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,19 +1,15 @@
 yay -S --noconfirm --needed \
-  brave-bin
-# gnome-calculator #  keepassxc qt5-wayland \
-#  nautilus \
-#  nextcloud-client
-# wl-clip-persist clipse \
-# chromium vlc # fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
-# nautilus sushi gnome-calculator \
-#  keepassxc-qt6 #   brightnessctl playerctl pamixer pavucontrol wireplumber \
-#   evince imv
+  brave-bin keepassxc nextcloud-client qt5-wayland \
+  gnome-calculator nautilus flameshot vlc \
+  fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt \
+  brightnessctl evince imv \
+  wl-clip-persist clipse sushi pamixer playerctl pavucontrol wireplumber
 
 # Needed to pre-install keepassxc plugin in brave-bin
 
-# if command -v keepassxc &>/dev/null; then
-#   if [ -f /opt/brave-bin/brave ]; then
-#     echo '{ "external_update_url": "https://clients2.google.com/service/update2/crx" }' >/tmp/oboonakemofpalcgghocfoadofidjkkk.json
-#     sudo mkdir -p /opt/brave-bin/extensions && sudo /tmp/oboonakemofpalcgghocfoadofidjkkk.json /opt/brave-bin/extensions
-#   fi
-# fi
+if command -v keepassxc &>/dev/null; then
+  if [ -f /opt/brave-bin/brave ]; then
+    echo '{ "external_update_url": "https://clients2.google.com/service/update2/crx" }' >/tmp/oboonakemofpalcgghocfoadofidjkkk.json
+    sudo mkdir -p /opt/brave-bin/extensions && sudo /tmp/oboonakemofpalcgghocfoadofidjkkk.json /opt/brave-bin/extensions
+  fi
+fi
