31832ab329d8a80d938df92343fb76528bf17047
user <user@github.com>
2025-07-15 06:59:39 +0200

    fixes file error for bat-extras

--- install/03-terminal.sh
+++ install/03-terminal.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
   wget curl unzip inetutils \
-  fd eza fzf ripgrep zoxide bat batman \
+  fd eza fzf ripgrep zoxide bat bat-extras \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate \
   alacritty zsh stow \
