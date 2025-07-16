9643dc7b7955078084c3c6e9987a138c23858910
user <user@github.com>
2025-07-16 20:49:55 +0200

    adds webapps for certain channels

--- install/90-webapps.sh
+++ install/90-webapps.sh
@@ -1,4 +1,6 @@
 source ~/.local/share/dotfiles/zsh/functions.sh
 web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
-web2app "NOS" https://nos.nl/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/nos.png
+web2app "Tijdloze" https://www.vrt.be/vrtmax/livestream/audio/tijdloze/
+web2app "Vuurland" https://www.vrt.be/vrtmax/livestream/audio/vuurland/
+web2app "NOS" https://nos.nl/
