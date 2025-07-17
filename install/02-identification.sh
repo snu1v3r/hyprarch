b4a72ed65f0fc2a6d1bf869d825d99f9935e812c
user <user@github.com>
2025-07-17 20:32:25 +0200

    adds profile selection
    Adds profile selection to the install script
    - profile can be selected on the cli with the --profile argument
    - profile can also be selected through a menu

--- install/02-identification.sh
+++ install/02-identification.sh
@@ -5,3 +5,15 @@ yay -S --noconfirm --needed gum
 echo -e "\nEnter identification for git and autocomplete..."
 export HYPRARCH_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
 export HYPRARCH_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
+
+# An optional selection for the profile if not already given
+if [ "$PROFILE" = "" ]; then
+  RESULT=$(gum choose Main Hacking Server --header="Select the target profile:" --timeout=5s)
+  if [ "$RESULT" = "" ]; then
+    PROFILE="main"
+  else
+    PROFILE=$(echo "$RESULT" | tr '[:upper:]' '[:lower:]')
+  fi
+fi
+
+echo $PROFILE
