4474ee50fc004b5aed3bbd8a78e403db20e0d99a
user <user@github.com>
2025-07-18 15:22:29 +0200

    adds grub theming

--- install/80-grub.sh
+++ install/80-grub.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
+
+sudo mkdir -p /boot/grub/themes
+sudo mv ~/.local/share/hyprarch/themes/grub/SekiroShadow /boot/grub/themes
+
+# This ensures that the theme is configured
+sudo sed -i "s/^#\?\(GRUB_THEME=\).*$/\\1\"\/boot\/grub\/themes\/SekiroShadow\/theme.txt\"/" /etc/default/grub
+
+# This ensures that grub uses a higher graphix resolution
+sudo sed -i "s/^#\?\(GFX_MODE=\).*$/\\11920x1080/" /etc/default/grub
+
 sudo sed -i "s/\(GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]*\)/\\1 splash/" /etc/default/grub
+# Update grub to enable updates
+sudo grub-mkconfig -o /boot/grub/grub.cfg
