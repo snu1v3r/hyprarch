15cd492dcd37694f5b82c1599281359ecee93c0a
user <user@github.com>
2025-07-18 15:20:55 +0200

    removes grub update to dedicated script

---
+++ install/80-grub.sh
@@ -0,0 +1,1 @@
+sudo sed -i "s/\(GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]*\)/\\1 splash/" /etc/default/grub
--- install/80-plymouth.sh
+++ install/80-plymouth.sh
@@ -10,9 +10,5 @@ sudo mv ~/.local/share/hyprarch/themes/plymouth/Error /usr/share/plymouth/themes
 
 sudo plymouth-set-default-theme Error -R
 
-# Update grub to enable loading of splash screen as part of the boot
-sed "s/\(GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]*\)/\\1 splash/" /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
 # Needed to ensure that plymouth is loaded as early as possible
 sudo mkinitcpio -A plymouth -g /boot/initramfs-linux.img
