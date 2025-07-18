69ebddbc924521d6ab351e1359f679f83c03bf6f
user <user@github.com>
2025-07-18 17:45:39 +0200

    fixes error in sed command

--- install/80-grub.sh
+++ install/80-grub.sh
@@ -7,7 +7,7 @@ sudo mv ~/.local/share/hyprarch/themes/grub/SekiroShadow /boot/grub/themes
 sudo sed -i "s/^#\?\(GRUB_THEME=\).*$/\\1\"\/boot\/grub\/themes\/SekiroShadow\/theme.txt\"/" /etc/default/grub
 
 # This ensures that grub uses a higher graphix resolution
-sudo sed -i "s/^#\?\(GFX_MODE=\).*$/\\11920x1080/" /etc/default/grub
+sudo sed -i "s/^#\?\(GRUB_GFXMODE=\).*$/\\11920x1080/" /etc/default/grub
 
 sudo sed -i "s/\(GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]*\)/\\1 splash/" /etc/default/grub
 # Update grub to enable updates
